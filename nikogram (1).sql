e1f10a3dffddee2077086d09259cd0c0076b82a6
Chrisss967 <castrejonchristopher478@gmail.com>
2026-04-15 08:11:38 -0700

    Add files via upload

---
+++ nikogram (1).sql
@@ -0,0 +1,245 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-04-2026 a las 17:11:18
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `nikogram`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `comentario_id` int(11) NOT NULL,
+  `pub_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `texto` varchar(300) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hashtags`
+--
+
+CREATE TABLE `hashtags` (
+  `hashtag_id` int(11) NOT NULL,
+  `etiqueta` varchar(60) NOT NULL,
+  `creado_en` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publicaciones`
+--
+
+CREATE TABLE `publicaciones` (
+  `pub_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `imagen_url` varchar(255) NOT NULL,
+  `fecha_pub` datetime NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `publicaciones`
+--
+
+INSERT INTO `publicaciones` (`pub_id`, `usuario_id`, `contenido`, `imagen_url`, `fecha_pub`, `likes`) VALUES
+(1, 3, 'Toy en la horny jail', 'https://preview.redd.it/katsunori-and-yo-chan-appreciation-post-v0-uljew12lh2og1.jpeg?width=736&format=pjpg&auto=webp&s=6b7c05417af8c43c34c9da012ea116caeee3473f', '2026-04-15 16:45:42', 0),
+(2, 1, 'blow me ', 'https://in.pinterest.com/pin/1079315866949305244/', '2026-04-15 16:45:42', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pub_hashtag`
+--
+
+CREATE TABLE `pub_hashtag` (
+  `pub_id` int(11) NOT NULL,
+  `hashtag_id` int(11) NOT NULL,
+  `PRIMARY_KEY` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pub_likes`
+--
+
+CREATE TABLE `pub_likes` (
+  `pub_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `fecha_like` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seguidores`
+--
+
+CREATE TABLE `seguidores` (
+  `seguidor_id` int(11) NOT NULL,
+  `seguido_id` int(11) NOT NULL,
+  `fecha_follow` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `seguidores`
+--
+
+INSERT INTO `seguidores` (`seguidor_id`, `seguido_id`, `fecha_follow`) VALUES
+(2, 3, '2012-09-11'),
+(3, 2, '2012-09-11'),
+(2, 4, '2025-03-22'),
+(3, 4, '2025-03-22'),
+(1, 5, '2024-02-14');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `bio` varchar(200) NOT NULL,
+  `fecha_reg` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `username`, `email`, `bio`, `fecha_reg`) VALUES
+(1, 'Niko Toba', 'nikolastobias@gmail.com', 'Nikolas tobias\r\nToba aquarium\r\n6 años\r\nI like to eat cement', '2020-02-22'),
+(2, 'Yo chan', 'yochangyu@gmail.com', 'gyu', '2010-04-10'),
+(3, 'Katsunori', 'cachondori@gmail.com', 'kachan', '2012-06-09'),
+(4, 'tsuki', 'tsuki@gmail.com', 'guh', '2025-03-21'),
+(5, 'Kyoro Kyoro', 'kyorokyoro@gmail.com', 'lesbiana', '2016-06-14');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`comentario_id`);
+
+--
+-- Indices de la tabla `hashtags`
+--
+ALTER TABLE `hashtags`
+  ADD PRIMARY KEY (`hashtag_id`),
+  ADD UNIQUE KEY `etiqueta` (`etiqueta`);
+
+--
+-- Indices de la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  ADD PRIMARY KEY (`pub_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `pub_likes`
+--
+ALTER TABLE `pub_likes`
+  ADD KEY `pub_id` (`pub_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `seguidores`
+--
+ALTER TABLE `seguidores`
+  ADD KEY `seguidor_id` (`seguidor_id`),
+  ADD KEY `seguido_id` (`seguido_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `comentario_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `hashtags`
+--
+ALTER TABLE `hashtags`
+  MODIFY `hashtag_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  MODIFY `pub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  ADD CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`);
+
+--
+-- Filtros para la tabla `pub_likes`
+--
+ALTER TABLE `pub_likes`
+  ADD CONSTRAINT `pub_likes_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `publicaciones` (`pub_id`),
+  ADD CONSTRAINT `pub_likes_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`);
+
+--
+-- Filtros para la tabla `seguidores`
+--
+ALTER TABLE `seguidores`
+  ADD CONSTRAINT `seguidores_ibfk_1` FOREIGN KEY (`seguidor_id`) REFERENCES `usuarios` (`usuario_id`),
+  ADD CONSTRAINT `seguidores_ibfk_2` FOREIGN KEY (`seguido_id`) REFERENCES `usuarios` (`usuario_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
